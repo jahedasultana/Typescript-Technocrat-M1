b0bf90cddf1ef88ee06c04340e0110ca43619274
jaheda8256 <jahedasultana47@gmail.com>
2025-04-01 10:00:00 +0600

    Union type implement

---
+++ module-1/module/src/1.10.ts
@@ -0,0 +1,43 @@
+{
+    // union type
+
+//     type FrontendDeveloper = 'fakibazDevloper' | 'juniorDeveloper'
+//     type FullstackDeveloper = 'frontentDeveloper' | 'expertDeveloper'
+
+//     type Developer = FrontendDeveloper | FullstackDeveloper
+
+//     const newDeveloper : FrontendDeveloper = 'juniorDeveloper'
+
+
+//     type User = {
+//         name: string;
+//         email?: string;
+//         gender: "male"| "female";
+//         bloodGroup: "O+"|"A+"|"AB+";
+//     };
+
+// const user1: User = {
+//     name:'persian',
+//     gender: 'male',
+//     bloodGroup: 'O+'
+// };
+
+// intersection types
+
+type FrontendDeveloper = {
+    skills: string[];
+    designation1:'Frontend Developer' 
+}
+type BackendDeveloper = {
+    skills: string[];
+    designation2:'Backend Developer' 
+}
+ type FullstackDeveloper = FrontendDeveloper | BackendDeveloper
+
+ const fullstackDeveloper : FullstackDeveloper = {
+    skills: ['Html','Css','Express'],
+    designation1: 'Frontend Developer',
+    designation2: 'Backend Developer'
+ }
+
+}
